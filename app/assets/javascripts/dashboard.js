282c071aa60f12efe9ca3a837fc2b72f80a9375c
besmart <aliadas233@gmail.com>
2016-02-03 20:42:26 -0500

    Datos reales de la Base de Datos

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,65 +1,44 @@
 
 
 $(document).on('page:change', function(){
-	Morris.Donut({
+	$.getJSON('/dashboard/distribucion_agencia.json', function(data){
+		Morris.Donut({
 
-		element: 'distribucion-agencia',	
-		data: [
-
-			{label: 'Parque de Berrio', value:50 },
-			{label: 'Punto Clave', value:30 },	
-			{label: 'Gran Plaza', value:5 }
-		],
-		colors: [ '#256c3c', '#a2673a', '#286469']
+			element: 'distribucion-agencia',	
+			data: data,
+			colors: [ '#256c3c', '#a2673a', '#286469']
+		});	
+    });	
+	$.getJSON('/dashboard/distribucion_estado.json', function(data){
+		Morris.Donut({
+			element: 'distribucion-estado',		
+			data: data,
+		});
 	});	
-
-	Morris.Donut({
-		element: 'distribucion-estado',		
-		data: [
-			{ label: 'Recepción', value: 100 },
-			{ label: 'Atención', value: 200 },
-			{ label: 'Atendido', value: 500 }
-		]
-
-	});
 	
-	Morris.Line({
+	$.getJSON('/dashboard/promedio_espera.json', function(data){
+		Morris.Line({
 
-		element: 'promedio-espera',
-		data: [
-			{ day: '2016-01-01', value: 3 },
-			{ day: '2016-01-02', value: 4 },
-			{ day: '2016-01-03', value: 5 },
-			{ day: '2016-01-04', value: 6 },
-			{ day: '2016-01-05', value: 7 },
-			{ day: '2016-01-06', value: 8 }
-			
-		],
-		xkey: 'day',
-		ykeys: ['value'],		
-		labels: ['value']
+			element: 'promedio-espera',
+			data: data,
+		
+			xkey: 'day',
+			ykeys: ['value'],
+			labels: ['value']
 
-
+		});	
 	});		
 
-	Morris.Line({
+	$.getJSON('/dashboard/promedio_atencion.json', function(data){
+		Morris.Line({
 
-		element: 'promedio-atencion',
-		data: [
-			{ day: '2016-01-01', value: 3 },
-			{ day: '2016-01-02', value: 4 },
-			{ day: '2016-01-03', value: 5 },
-			{ day: '2016-01-04', value: 6 },
-			{ day: '2016-01-05', value: 7 },
-			{ day: '2016-01-06', value: 8 }
-			
-		],
+			element: 'promedio-atencion',
+			data: data,
 
-		xkey: 'day',
-		ykeys: ['value'],
-		labels: ['value']
+			xkey: 'day',
+			ykeys: ['value'],
+			labels: ['value']
 
-
-	})
-
+		});
+	});
 });
---
+++ app/views/dashboard/distribucion_estado.json.jbuilder
@@ -0,0 +1,5 @@
+json.array! @estados do |estado,turnos|
+	json.label estado
+	json.value turnos.count
+end
+
